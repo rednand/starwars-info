fc8cab56ee9c9acbc1a8a205d870846b7ddfeaec
rednand <nan-aguiar@hotmail.com>
2022-06-14 22:05:55 -0300

    pagination

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -3,38 +3,61 @@ import { useState } from "react";
 import Planet from "./Planet";
 import { ReactQueryDevtools } from "react-query/devtools";
 
-const fetchPlanets = async (key, greeting, page) => {
-  console.log(greeting);
-  const result = await fetch(`https://swapi.dev/api/planets/?page=${page}`);
-  return result.json();
-};
-
 const queryClient = new QueryClient();
 
 const Planets = () => {
   const [page, setPage] = useState(1);
-  const { data, status } = useQuery(["planets", "hello,ninjas", page], fetchPlanets);
+
+  const fetchPlanets = (page = 0) =>
+    fetch("http://swapi.dev/api/planets/?page=" + page).then((res) =>
+      res.json()
+    );
+
+  const {
+    data,
+    status,
+    isLoading,
+    isError,
+    error,
+  } = useQuery(["planets", "hello,ninjas", page], () => fetchPlanets(page), {
+    keepPreviousData: true,
+  });
   console.log("data", data, "status", status);
 
   return (
     <>
       <div>
         <h2>Planets</h2>
-
-        <button onClick={() => setPage(3)}>page 3</button>
-        {status === "loading" && <div>Loading data ...</div>}
-
-        {status === "error" && <div>Error fetching data</div>}
-
-        {status === "success" && (
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : isError ? (
+          <div>Error: {error.message}</div>
+        ) : (
           <div>
-            {data.results.map((planet) => (
-              <Planet key={planet.name} planet={planet} />
-            ))}
+            <>
+              <button
+                onClick={() => setPage((old) => Math.max(old - 1, 1))}
+                disabled={page === 1}
+              >
+                Previous Page
+              </button>
+              <span>{page}</span>
+              <button
+                onClick={() =>
+                  setPage((old) => (!data || !data.next ? old : old + 1))
+                }
+                disabled={!data || !data.next}
+              >
+                Next Page
+              </button>
+              {data.results.map((planet) => (
+                <Planet key={planet.name} planet={planet} />
+              ))}
+            </>
           </div>
         )}
+        <ReactQueryDevtools initialIsOpen={false} />
       </div>
-      <ReactQueryDevtools initialIsOpen={false} />
     </>
   );
 };
